a7e87bc13783fb521f343418075f275bcc2f3ad6
Ricael Lopes <ricael@zscansoftware.com>
2023-08-14 13:48:54 -0300

    <Feat>-4880759141---Esconder-Saldo

--- cypress/e2e/esconder-saldo.cy.js
+++ cypress/e2e/esconder-saldo.cy.js
@@ -1,5 +1,6 @@
 describe('Acessar homepage', () => {
     it('Acessar site', () => {
         cy.visit('https://attalho-financas-v110.vercel.app/')
+        cy.contains('span', 'Esconder saldo das contas').click()
     });
 });
